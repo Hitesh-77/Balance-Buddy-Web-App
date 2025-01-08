3763ffa7fb4358ab70fe393deea236aa9d594a61
Hitesh-77 <hitesh2182006@gmail.com>
2025-01-08 21:43:57 +0530

    new changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let btn1=document.querySelector("#btn1");
 let btn2=document.querySelector("#btn2");
+let btn3=document.querySelector("#btn3");
+let balance=0;
 
 btn1.addEventListener("click",()=>
 {
@@ -7,7 +9,7 @@ btn1.addEventListener("click",()=>
     let newDate = day.value;
 
     let amt = document.querySelector("#amt");
-    let newAmt = amt.value;
+    let newAmt = parseFloat(amt.value);
     
     let cate = document.querySelector("#Category");
     let category = cate.value;
@@ -16,9 +18,10 @@ btn1.addEventListener("click",()=>
     {
         let [year,month,date] = newDate.split("-");
         let formatDate = `${date}-${month}-${year}`;
-        
+        balance += (newAmt);
 
         addTransactions(category,"Income",newAmt,formatDate);
+        trackBalance(balance);
     }
     else
     {
@@ -33,7 +36,7 @@ btn2.addEventListener("click",()=>
         let newDate = day.value;
     
         let amt = document.querySelector("#amt");
-        let newAmt = amt.value;
+        let newAmt = parseFloat(amt.value);
         
         let cate = document.querySelector("#Category");
         let category = cate.value;
@@ -42,9 +45,10 @@ btn2.addEventListener("click",()=>
         {
             let [year,month,date] = newDate.split("-");
             let formatDate = `${date}-${month}-${year}`;
-            
+            balance-= newAmt;
     
             addTransactions(category,"Expense",newAmt,formatDate);
+            trackBalance(balance);
         }
         else
         {
@@ -52,12 +56,41 @@ btn2.addEventListener("click",()=>
         }
         
     });
+
+btn3.addEventListener("click",()=>
+{
+    let day = document.querySelector("#day");
+    day.value = "";
     
+    let amt = document.querySelector("#amt");
+    amt.value = "";
+        
+    let cate = document.querySelector("#Category");
+    cate.value = "Select";
+});
 function addTransactions(category,type,newAmt,formatDate)
 {
     let row = document.createElement("tr");
-    row.innerHTML = `<td>${category}</td><td>${type}</td><td>$${newAmt}</td><td>${formatDate}</td>`;
+    if(type == "Income")
+    {row.innerHTML = `<td>${category}</td><td>${type}</td><td>$${newAmt}</td><td>${formatDate}</td><td><button class="cross">X</button></td>`;}
+    else
+    {row.innerHTML = `<td>${category}</td><td>${type}</td><td>($${newAmt})</td><td>${formatDate}</td><td><button class="cross">X</button></td>`;}
 
     let table = document.querySelector("table");
     table.appendChild(row);
+
+}
+
+function trackBalance(balance)
+{
+    let setBalance = document.querySelector(".balance h2");
+    if(balance >=0)
+    {
+        setBalance.innerHTML = `$${balance}`;
+    }
+    else
+    {
+        balance -= (2*balance);
+        setBalance.innerHTML = `($${balance})`;
+    }
 }
